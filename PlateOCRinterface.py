4cbb61c67e2d9105568a5be2a49f45cc58f1b3e1
ErenArkun <70512163+ErenArkun@users.noreply.github.com>
2024-03-18 03:19:22 +0300

    commit & push

--- PlateOCRinterface.py
+++ PlateOCRinterface.py
@@ -1,3 +1,7 @@
+"""
+Arduino serial communication project
+"""
+
 import cv2
 import serial.tools.list_ports
 from tkinter.ttk import *
@@ -116,7 +120,6 @@ def thd_box(plaka):
 def on():
     try:
         serial_port.write(b'H')
-        # box_print("34 IST 34")
         print("led acik")
 
 
@@ -135,26 +138,17 @@ led_on.place(relx=0.3, rely=0.85, relwidth=0.07, relheight=0.07)
 save_box.config(yscrollcommand=ListScrollbar.set)
 ListScrollbar.config(command=save_box.yview)
 
-#cap = cv2.VideoCapture(0)
-
-vid_path = 'C:\\Users\\eren2\\PycharmProjects\\OCR_Plate_SQL\\examples\\pr1.mp4'
-cap = cv2.VideoCapture(vid_path)
+cap = cv2.VideoCapture(0)
 while True:
-    # kamera görüntü oku
-
+    
     ret, original_image = cap.read()
-    # gelen görüntüyü boyutlandır
     original_image = imutils.resize(original_image, width=700)
-    # kameradan gelen görüntüyü göster
     # cv2.imshow("Orjinal goruntu", original_image)
 
-    # gelen görüntüyü gri tonlarına çevir
     gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     gray_image = cv2.bilateralFilter(gray_image, 11, 17, 17)
-    # gri tonlarında görüntüyü göster
     # cv2.imshow("Gray", gray_image)
 
-    # görüntüde sınırları bul
     edged_image = cv2.Canny(gray_image, 30, 200)
     contours, new = cv2.findContours(edged_image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     img1 = original_image.copy()
@@ -162,12 +156,11 @@ while True:
     # cv2.imshow("img1", img1)
 
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:30]
-    # stores the license plate contour
+
     screenCnt = None
 
-    # kameradan gelen görüntüyü img2 e kopyala
     img2 = original_image.copy()
-    # img2 kontur son9orlarını çiz ve göster
+
     cv2.drawContours(img2, contours, -1, (0, 255, 0), 3)
     # cv2.imshow("img2", img2)
 
@@ -175,34 +168,27 @@ while True:
     idx = 7
 
     for c in contours:
-        # approximate the license plate contour
         contour_perimeter = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.018 * contour_perimeter, True)
 
-        # Look for contours with 4 corners
         if len(approx) == 4:
             screenCnt = approx
 
-            # find the coordinates of the license plate contour
             x, y, w, h = cv2.boundingRect(c)
             new_img = original_image[y: y + h, x: x + w]
 
-            # stores the new image
             cv2.imwrite('./' + str(idx) + '.png', new_img)
             idx += 1
             break
     try:
-        # draws the license plate contour on original image
         #cv2.drawContours(original_image, [screenCnt], -1, (0, 255, 0), 3)
         if screenCnt is not None:
             cv2.drawContours(original_image, [screenCnt], -1, (0, 255, 0), 3)
         # cv2.imshow("detected license plate", original_image)
 
-        # filename of the cropped license plate image
         cropped_License_Plate = './7.png'
         # cv2.imshow("cropped license plate", cv2.imread(cropped_License_Plate))
 
-        # converts the license plate characters to string
         #text = pytesseract.image_to_string(cropped_License_Plate, lang='eng', config='--psm 7')
         text = pytesseract.image_to_string(cropped_License_Plate, lang='eng', config=tessdata_dir_config)
         # print(type(text))
